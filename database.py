2f2c59708eb3f07ea1ed937b6ca24e2c556d9231
Franklin Sayago <franklin.solandy@gmail.com>
2025-01-28 12:41:49 -0500

    Adding Files

---
+++ database.py
@@ -0,0 +1,12 @@
+import mysql.connector
+
+database = mysql.connector.connect(
+    host='b0ilsbp5axpf1cdf0y8e-mysql.services.clever-cloud.com',
+    user='uhwtvgcnoburkmaj',
+    password='6cPBd5hT6j3HSQHgDf34',
+    database='b0ilsbp5axpf1cdf0y8e'
+#    host='localhost',
+#    user='root',
+#    password='',
+#    database='testingdb'
+)
